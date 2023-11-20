fecc42032266763c9b8abf9291bd0139686490b0
gkotov <georgi.kotov@dss.bg>
2023-11-20 08:56:22 +0200

    add post method for ex2

--- src/student-ex-2/student-ex2.controller.ts
+++ src/student-ex-2/student-ex2.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { StudentServiceEx2 } from './student-ex2.service';
 
 @Controller('student-ex2')
@@ -8,5 +8,10 @@ export class StudentControllerEx2 {
   @Get()
   getStudents() {
     return this.studentService.findAll();
+  }
+
+  @Post()
+  createStudent(@Body() student) {
+    return this.studentService.createOne(student);
   }
 }
