6d1ec39c85ecc4ceee9afaca2e10d4b1f268593b
gkotov <georgi.kotov@dss.bg>
2023-12-04 07:30:26 +0200

    feat: add university feature

---
+++ src/university-ex-3/university.entity.ts
@@ -0,0 +1,11 @@
+import { Student } from 'src/student-ex-2/student.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+
+@Entity()
+export class University {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  name: string;
+}
